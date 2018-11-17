0c5bf2d053c3841ed105823a9deb1016778df735
Thomas Chamelot <chamelot.thomas@gmail.com>
2018-11-17 13:39:45 +0100

    Fix compilation flags for stack protection. GP fault after few seconds

--- utils/config.mk
+++ utils/config.mk
@@ -14,7 +14,8 @@ CFLG_FP    := -mno-mmx -mno-sse -mno-sse2 -mno-sse3 -mno-ssse3 -mno-sse4.1 \
               -mno-sse4.2 -mno-sse4 -mno-avx -mno-avx2 -mno-aes -mno-pclmul \
               -mno-fsgsbase -mno-rdrnd -mno-f16c -mno-fma -mno-sse4a \
               -mno-fma4 -mno-xop -mno-lwp -mno-3dnow -mno-popcnt \
-              -mno-abm -mno-bmi -mno-bmi2 -mno-lzcnt -mno-tbm
+              -mno-abm -mno-bmi -mno-bmi2 -mno-lzcnt -mno-tbm \
+			  -fno-stack-protector
 
 CFLG_32    := -m32 -g -fno-pic
 CFLG_WRN   := -Wall -W -Werror
